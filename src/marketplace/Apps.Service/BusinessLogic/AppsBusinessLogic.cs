8304cc6eaa35fed0b5c0ea49d1fe508f8fbf2b44
VPrasannaK94 <prasannakumar.v@partner.bmw.in>
2023-03-02 23:19:20 +0530

    feat : newendpoint-post offerAssignedAppLeadImageDocument
    upload AppleadImage Document , update existing
    AppLeadImage Document Status to InActive and remove it.
    
    Ref : CPLP-1555

--- src/marketplace/Apps.Service/BusinessLogic/AppsBusinessLogic.cs
+++ src/marketplace/Apps.Service/BusinessLogic/AppsBusinessLogic.cs
@@ -33,6 +33,7 @@ using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities.Entities;
 using Org.Eclipse.TractusX.Portal.Backend.PortalBackend.PortalEntities.Enums;
 using System.Text.Json;
 using Org.Eclipse.TractusX.Portal.Backend.Mailing.SendMail;
+using System.Security.Cryptography;
 
 namespace Org.Eclipse.TractusX.Portal.Backend.Apps.Service.BusinessLogic;
 
@@ -378,5 +379,66 @@ public class AppsBusinessLogic : IAppsBusinessLogic
             throw new UnexpectedConditionException("offerDescriptionDatas should never be null here");
         }
         return result.OfferDescriptionDatas;
+    }
+
+    /// <inheritdoc />
+    public async Task CreatOfferAssignedAppLeadImageDocumentByIdAsync(Guid appId, string iamUserId, IFormFile document, CancellationToken cancellationToken)
+    {
+        var appLeadImageContentTypes = new []{ "image/jpeg","image/png" };
+        if (!appLeadImageContentTypes.Contains(document.ContentType))
+        {
+            throw new UnsupportedMediaTypeException($"Document type not supported. File with contentType :{string.Join(",", appLeadImageContentTypes)} are allowed.");
+        }
+
+        var offerRepository = _portalRepositories.GetInstance<IOfferRepository>();
+        var result = await offerRepository.GetOfferAssignedAppLeadImageDocumentsByIdAsync(appId, iamUserId, OfferTypeId.APP).ConfigureAwait(false);
+
+        if (!result.IsStatusActive)
+        {
+            throw new ConflictException($"offerStatus is in Incorrect State");
+        }
+        var companyUserId = result.CompanyUserId;
+        if (companyUserId == Guid.Empty)
+        {
+            throw new ForbiddenException($"user {iamUserId} is not a member of the providercompany of Apps {appId}");
+        }
+
+        var documentRepository = _portalRepositories.GetInstance<IDocumentRepository>();
+        if(result.documentStatusDatas.Any())
+        {
+            foreach(var documentData in result.documentStatusDatas)
+            {
+                documentRepository.AttachAndModifyDocument(documentData.DocumentId,
+                    A => { A.DocumentStatusId = documentData.StatusId; },
+                    A => { A.DocumentStatusId = DocumentStatusId.INACTIVE; }
+                );
+            }
+            await _portalRepositories.SaveAsync().ConfigureAwait(false);
+            _portalRepositories.Clear();
+        }
+        var documentName = document.FileName;
+        using var sha512Hash = SHA512.Create();
+        using var ms = new MemoryStream((int)document.Length);
+
+        await document.CopyToAsync(ms, cancellationToken).ConfigureAwait(false);
+        var hash = sha512Hash.ComputeHash(ms);
+        var documentContent = ms.GetBuffer();
+        if (ms.Length != document.Length || documentContent.Length != document.Length)
+        {
+            throw new ControllerArgumentException($"document {document.FileName} transmitted length {document.Length} doesn't match actual length {ms.Length}.");
+        }
+        var doc = documentRepository.CreateDocument(documentName, documentContent, hash, DocumentTypeId.APP_LEADIMAGE, x =>
+        {
+            x.CompanyUserId = companyUserId;
+            x.DocumentStatusId = DocumentStatusId.LOCKED;
+        });
+        _portalRepositories.GetInstance<IOfferRepository>().CreateOfferAssignedDocument(appId, doc.Id);
+
+        foreach(var docID in result.documentStatusDatas)
+        {
+            offerRepository.RemoveOfferAssignedDocument(appId, docID.DocumentId);
+            documentRepository.RemoveDocument(docID.DocumentId);
+        }
+        await _portalRepositories.SaveAsync().ConfigureAwait(false);
     }
 }
